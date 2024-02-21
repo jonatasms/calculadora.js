7055d60f6bec4235725724b2da99c47ecc164ccd
Jônatas <104945169+jonatasms@users.noreply.github.com>
2024-02-21 20:42:55 -0300

    .

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ var soma = num1 + num2
 //exibir  a resposta!
 alert ("o resultado da soma é: " + soma)
 document.write(soma)
+
+
+//subtração
+var subtr = num1 - num2
+alert ("o resultado da subtração é:"+ " "+ subtr)
+document.write("<br>A subtração entre os numeros foi :"+ " "+ subtr)
