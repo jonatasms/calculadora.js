2210c5d38f2cd5d73e295ddf963e1800374bf12c
Jônatas <104945169+jonatasms@users.noreply.github.com>
2024-03-16 17:18:28 -0300

    11commit

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-
-let num1 = parseInt(prompt("digite o primeiro numero!"))
-let num2 = parseInt(prompt("digite o segundo numero")) 
-
-//operação de soma!!
-var soma = num1 + num2
-
-//exibir  a resposta!
-alert ("o resultado da soma é: " + soma)
-document.write(soma)
-
-
-//subtração
-var subtr = num1 - num2
-alert ("o resultado da subtração é:"+ " "+ subtr)
-document.write("<br>A subtração entre os numeros foi :"+ " "+ subtr)
-
-
-//maniplação class.
+function calculator(tipo, valor){
+    console.log(tipo, valor)
+}
