d2b87181b06f43d1b8bcf266ac23bf82136f6055
Jônatas <104945169+jonatasms@users.noreply.github.com>
2024-03-16 23:36:33 -0300

    commit13

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function calculator(tipo, valor){
+    if(tipo === 'acao'){
+        //limperesultado no visor!! 
+        if(valor === 'C') {
+            document.getElementById('resultado').value ="";
+        }
+        //declarando a ação dos botões de operações
+        if(valor === '+'||valor === '-'||valor === '*'||valor === '/'||valor === '.'){
+            document.getElementById('resultado').value += valor;
+        }
+        //função que realiza as contas!
+        if(valor === '='){
+            var campoDisplay = eval(document.getElementById("resultado").value);
+            document.getElementById('resultado').value = campoDisplay;
+        }
+    }else if(tipo === 'valor'){
+        //var campoDisplay = document.getElementById('resultado').value;
+        //document.getElementById('resultado').value = campoDisplay + valor;
+        //enchugando o código anterior e coloca abaixo 
+        document.getElementById('resultado').value += valor;
+    }
+    }
     console.log(tipo, valor)
 }
